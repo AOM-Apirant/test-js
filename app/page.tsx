ac133d2442148a334a1f317b91c59aa830800510
AOM-Aspirant <aomaspirant@gmail.com>
2025-06-20 23:16:29 +0530

    about page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
+  );
 }
 
 export default Home
